ac23c1515f488b5a81d1dd52785ec1391a369439
Nate Giesing <nategiesing@Nates-MacBook-Pro.local>
2017-07-15 09:53:48 -0600

    Started boarded and computer

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+
+class Board
+
+  def grid
+    board = {
+              bs: [" ", " *BATTLESHIP*", " "],
+              n:  [" ", "1", "2", "3", "4"],
+              a:  ["A", " ", " ", " ", " "],
+              b:  ["B", " ", " ", " ", " "],
+              c:  ["C", " ", " ", " ", " "],
+              d:  ["D", " ", " ", " ", " "]
+            }
+  end
+
+  def grid_validator(row, column)
+    if ["a", "b", "c", "d"].include?(row)
+      if ["1", "2", "3", "4"].include?(column)
+        true
+    else
+      invalid_message
+      end
+    end
+  end
+
+  def placement_validator(key, index)
+    possible_position = []
+    if key != :a
+      row = (key.to_s.ord-1).chr.to_sym
+      possible_position << [row, index]
+    end
+    if index != 4
+      column = index +1
+      possible_position << [key, column]
+    end
+    if key != :d
+      row = (key.to_s.ord+1).chr.to_sym
+      possible_position << [row, index]
+    end
+    if index != 1
+      column = index - 1
+      possible_position << [key, column]
+    end
+    # require "pry"; binding.pry
+    second_position = possible_position.sample
+    grid_writer(second_position[0], second_position[1])
+  end
+end
---
+++ test/board_test.rb
@@ -0,0 +1,38 @@
+require './test/test_helper'
+
+class BoardTest < Minitest::Test
+
+
+  def test_does_it_make_a_grid
+      board = Board.new
+      actual = board.grid
+      expected =   {
+      bs: [" ", " *BATTLESHIP*", " "],
+      n:  [" ", "1", "2", "3", "4"],
+      a:  ["A", " ", " ", " ", " "],
+      b:  ["B", " ", " ", " ", " "],
+      c:  ["C", " ", " ", " ", " "],
+      d:  ["D", " ", " ", " ", " "]
+      }
+
+      assert_equal expected, actual
+  end
+
+  def test_cant_place_on_invalid_board_area
+    board = Board.new
+    row = "e"
+    column = "5"
+
+    refute board.grid_validator(row, column)
+  end
+
+  def test_can_only_place_on_valid_board_area
+    board = Board.new
+    row = "a"
+    column = "3"
+    assert board.grid_validator(row, column)
+  end
+
+  
+
+end
